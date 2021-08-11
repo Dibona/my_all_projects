455aadb60c490eaf7fe780457c689263050d85b6
unknown <muni787626@gmail.com>
2021-08-11 17:21:20 +0600

    adadasda

---
+++ minibiral v1.0.py
@@ -0,0 +1,13 @@
+import os
+file_name = input("Folder name: ")
+f = int(input("folder number: "))
+for i in range(f):
+	os.system(f"mkdir {file_name}{i}")
+z = input("delete folder ?/: ")
+
+if z == "yes":
+	for i in range(f):
+		os.system(f"rmdir {file_name}{i}")
+else:
+	print("Thanks for using my program")	
+	
