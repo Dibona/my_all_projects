b0102c6e1982fd39994b750a795edbd3c123ef1d
unknown <muni787626@gmail.com>
2021-08-16 10:18:48 +0600

    2021-08-16 10:18:45.080974

--- pushing.py
+++ pushing.py
@@ -1,5 +1,15 @@
 import os
+from datetime import datetime
+time = str(datetime.now())
+x = input("commit name")
+
+
+if x == "":
+    x = time
+command = f"git commit -m \"{x}\""
+print(command)
 
 os.system("git add .")
-os.system("git commit -m \"abcd\"")
+os.system(command)
 os.system("git push -u origin master")
+
